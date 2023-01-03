26b65bb00f071eed0d2cc1207b951d1cfc81a210
Aobise <splasherdon@gmail.com>
2023-01-03 00:08:21 +0000

    update

--- bj101.sh
+++ bj101.sh
@@ -2,22 +2,23 @@
 
 # update instance 
 
-echo sudo apt update 
+sudo apt update 
 sleep 5
 
 #set up an instance to use docker commands 
 
-echo sudo apt-get install -y wget
+sudo apt-get install -y wget
 sleep 5
 
-echo sudo apt-get install -y python
+sudo apt-get install -y python
 sleep 5
 
-echo sudo apt-get install docker.io
+sudo apt-get install docker.io
 sleep 5
 
-echo docker --version
+docker --version
 sleep 5
+
 
 
 
